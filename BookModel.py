f24b38c6b583e15431bcfb02cb67e85cd117bdc4
TikiHam <scouserakim@gmail.com>
2025-10-23 16:27:33 +0300

    No changes

--- BookModel.py
+++ BookModel.py
@@ -1,3 +1,11 @@
 from SQLAlchemy import Column, Integer, String
 from SQLAlchemy.orm import declarative_base
 from passlib import CryptContext
+
+Base = declarative_base()
+pwd_context = CryptContext(shcems=["bcrypt"], deprecated="auto")
+
+class User(Base):
+    __tablename__ = "users"
+    
+    id = Column(Integer)
